9f8193241967746844dc6a89825cb69e81f9760a
ulammatha <udaykumar1117@gmail.com>
2016-01-05 13:21:09 -0800

    add cancel button

--- vendor/vendor-ctrl.js
+++ vendor/vendor-ctrl.js
@@ -3,16 +3,29 @@ angular.module('dealApp').controller('VendorCtrl', ['$scope', 'Restangular', fun
 	vendorsList.getList().then(function(result) {
 		$scope.vendors = result;
 		$scope.editingData = {};
+		var oldData = {};
 		angular.forEach($scope.vendors, function(vendor){
-			$scope.editingData[vendor.email] = false;
+			$scope.editingData[vendor.id] = false;
 		});
 
-		$scope.edit = function(email){
-			$scope.editingData[email] = true;
+		$scope.edit = function(id, email, full_name){
+			$scope.editingData[id] = true;
+			oldData = {email: email, full_name: full_name}
 		};
 
-		$scope.done = function(email){
-			$scope.editingData[email] = false;
+		$scope.done = function(id){
+
+			$scope.editingData[id] = false;
+		};
+
+		$scope.cancel = function(vendor){
+			$scope.editingData[vendor.id] = false;
+			angular.forEach($scope.vendors, function(owner){
+				if(owner.id == vendor.id){
+					owner.email = oldData.email;
+					owner.full_name = oldData.full_name;
+				}
+			});
 		};
 	});
 }]);
