964c7937c8324ee66c690edc2d33ba2c780d9418
ulammatha <udaykumar1117@gmail.com>
2016-01-05 12:31:53 -0800

    add done button

--- vendor/vendor-ctrl.js
+++ vendor/vendor-ctrl.js
@@ -1,8 +1,7 @@
-angular.module('dealApp').controller('VendorCtrl', ['$scope', '$parse', 'Restangular', function($scope, $parse, Restangular){
+angular.module('dealApp').controller('VendorCtrl', ['$scope', 'Restangular', function($scope, Restangular){
 	var vendorsList = Restangular.all('vendors');
 	vendorsList.getList().then(function(result) {
 		$scope.vendors = result;
-
 		$scope.editingData = {};
 		angular.forEach($scope.vendors, function(vendor){
 			$scope.editingData[vendor.email] = false;
@@ -12,5 +11,8 @@ angular.module('dealApp').controller('VendorCtrl', ['$scope', '$parse', 'Restang
 			$scope.editingData[email] = true;
 		};
 
+		$scope.done = function(email){
+			$scope.editingData[email] = false;
+		};
 	});
 }]);
