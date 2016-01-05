c7c1c9f33b6e7559a3a05c9272161353a74a0f8f
ulammatha <udaykumar1117@gmail.com>
2016-01-04 18:00:27 -0800

    edit option for vendors list

--- vendor/vendor-ctrl.js
+++ vendor/vendor-ctrl.js
@@ -1,6 +1,16 @@
-angular.module('dealApp').controller('VendorCtrl', ['$scope', 'Restangular', function($scope, Restangular){
+angular.module('dealApp').controller('VendorCtrl', ['$scope', '$parse', 'Restangular', function($scope, $parse, Restangular){
 	var vendorsList = Restangular.all('vendors');
 	vendorsList.getList().then(function(result) {
 		$scope.vendors = result;
+
+		$scope.editingData = {};
+		angular.forEach($scope.vendors, function(vendor){
+			$scope.editingData[vendor.email] = false;
+		});
+
+		$scope.edit = function(email){
+			$scope.editingData[email] = true;
+		};
+
 	});
 }]);
